549a2a314646d700f1ae91f8fbe3aa49cba93e1a
Tuvshuu <87944616+tuvvshe@users.noreply.github.com>
2023-07-12 18:41:55 +0200

    Add files via upload

---
+++ print_emoji.py
@@ -0,0 +1,5 @@
+import emoji
+
+print(emoji.emojize("I love watching movies:movirs:"))
+print(emoji.emojize("Some people have a very sensitive heart:red_heart:, please be kind with them. : hibiscus:"))
+print(emoji.emojize("I love reading books:books"))
